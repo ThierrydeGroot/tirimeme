6d2d1bd73b3808903e0e59c4a8b7c0e06d4ae201
Piraato <lp.lennartpikijn@gmail.com>
2017-05-24 14:05:42 +0200

    updates verschillende bestanden

--- hidden.optionmenu.php
+++ hidden.optionmenu.php
@@ -17,12 +17,14 @@ foreach ($files as $link) {
 
     $heh = explode('.', $link);
 
-    if($heh[2] == 'php') {
-        $friendlylink = preg_replace('/[^A-Za-z0-9\-]/', '', $heh[1]);
+    if(!empty($heh[2])) {
+        if ($heh[2] == 'php' || $heh[2] == 'html') {
+            $friendlylink = preg_replace('/[^A-Za-z0-9\-]/', '', $heh[1]);
 
-        echo "<li><a href='$link'>" . ucfirst($friendlylink) ."</a></li>";
-    }else{
-        echo '';
+            echo "<li><a href='$link'>" . ucfirst($friendlylink) . "</a></li>";
+        } else {
+            echo '';
+        }
     }
 }
 
